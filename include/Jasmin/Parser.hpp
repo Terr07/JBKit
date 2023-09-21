aa4b5d69e5cba6f06e3c532d7ec20a6ba1d6b0c5
Terr07 <Terr07@proton.me>
2023-09-21 21:13:34 +0200

    Add cmake build script & parser header

---
+++ include/Jasmin/Parser.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Common.hpp"
+#include "Lexer.hpp"
+#include "AstNodes.hpp"
+
+//TODO: HIDE IN IMPLEMENTATION
+#include <fmt/core.h>
+
+namespace Jasmin
+{
+
+using namespace AstNodes;
+
+class Parser
+{
+  public:
+    Parser(std::queue<Lexeme> lexQueue);
+
+    bool HasMoreAfterSkip();
+    uPtr<Node> ParseNext();
+    std::vector< uPtr<Node> > ParseAll();
+
+  private:
+
+    uPtr<Node> parseDirective();
+    uPtr<Node> parseLabel(Lexeme labelName);
+    uPtr<Node> parseInstruction(Lexeme instrName);
+    Instruction::ArgT parseInstrArg();
+    uPtr<Value<double>> parseArithmeticExpr();
+
+    Lexeme pop();
+
+  private:
+    std::queue<Lexeme> lexemes;
+};
+
+
+} //namespace: Jasmin
